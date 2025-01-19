12d8d54064a8946d5db6abf295d7f878b78d367c
Gracjan Hankala <gracjanhankala@gmail.com>
2025-01-19 20:26:18 +0100

    dodanie zmiennej

--- shopping_list.py
+++ shopping_list.py
@@ -1,18 +1,3 @@
-# Zadanie lista zakupów
+zadanie_dodatkowe = "specjalne pozdrowienia!"
 
-zakupy = {
-    "piekarnia,": ["chleb", "bułki", "pączek", "rogalik",],
-    "warzywniak,": ["marchew", "seler", "rukola", "pomidor"],
-    "sklep wędkarski,": ["żyłka", "podbierak", "przynęta", "spławik"]
-}
-
-# Tworzenie pętli
-suma_produktów = 0
-for sklep, produkty in zakupy .items():
-    sklep_nazwa = sklep.title()
-    produkty_nazwa = ', '.join([produkt.title() for produkt in produkty])
-    print(f"Idę do {sklep_nazwa} kupuję tam następujące rzeczy: {produkty_nazwa}.")
-    suma_produktów += len(produkty)
-    
-# Suma produktów
-print(f"W sumie kupuję {suma_produktów} produktów.")
+print(zadanie_dodatkowe)
