33ad0b613bc096be14f001e6d0c9d57fb1a06b70
GracjanHankala <gracjanhankala@gmail.com>
2025-01-19 16:52:45 +0100

    Dodanie kom do pętli

--- shopping_list.py
+++ shopping_list.py
@@ -5,11 +5,13 @@ zakupy = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+# Tworzenie pętli
 suma_produktów = 0
 for sklep, produkty in zakupy .items():
     sklep_nazwa = sklep.title()
     produkty_nazwa = ', '.join([produkt.title() for produkt in produkty])
     print(f"Idę do {sklep_nazwa} kupuję am następujące rzeczy: {produkty_nazwa}.")
     suma_produktów += len(produkty)
+    
 # Suma produktów
 print(f"W sumie kupuję {suma_produktów} produktów.")
