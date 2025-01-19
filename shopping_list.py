38e6a75876f5be1af9b703e4e221baa5566dc841
GracjanHankala <gracjanhankala@gmail.com>
2025-01-19 16:40:57 +0100

    Dodanie kom do tekstu

--- shopping_list.py
+++ shopping_list.py
@@ -11,5 +11,5 @@ for sklep, produkty in zakupy .items():
     produkty_nazwa = ', '.join([produkt.title() for produkt in produkty])
     print(f"Idę do {sklep_nazwa} kupuję am następujące rzeczy: {produkty_nazwa}.")
     suma_produktów += len(produkty)
-
+# Suma produktów
 print(f"W sumie kupuję {suma_produktów} produktów.")
